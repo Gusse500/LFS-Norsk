8847acf975c9a4426f4a440c463647830382fa94
Karl-Gustav Freding <31445980+Gusse500@users.noreply.github.com>
2025-05-15 07:38:23 +0200

    Rettelser

--- lfs-latest-git.php
+++ lfs-latest-git.php
@@ -6,7 +6,11 @@ $vers = array();
 date_default_timezone_set( "GMT" );
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 $date = date( "d.m.Y H:i:s" );
+=======
+$date = date( "d.m Y H:i:s" );
+>>>>>>> parent of 14ac7e4 (Rettelser)
 =======
 $date = date( "d.m Y H:i:s" );
 >>>>>>> parent of 14ac7e4 (Rettelser)
